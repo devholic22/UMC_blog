0d62ad802c8a5a63a4caa8d5c5825f08b4415a57
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-05-12 12:06:07 +0900

    ✅ test: 글 생성 및 저장 테스트 구현
    - 임베디드 데이터베이스를 위한 H2 의존성 설치
    - 임베디드 데이터베이스 연결
    - 글 생성 및 저장 테스트 구현
    - 테스트에 필요한 application.yml 작성
    - 테스트에 필요한 schema.sql 작성

--- src/test/java/umc/blog/BlogApplicationTests.java
+++ src/test/java/umc/blog/BlogApplicationTests.java
@@ -2,6 +2,11 @@ package umc.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class BlogApplicationTests {
@@ -10,4 +15,14 @@ class BlogApplicationTests {
 	void contextLoads() {
 	}
 
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("min");
+		dataSource.setPassword("");
+		return dataSource;
+	}
 }
---
+++ src/test/java/umc/blog/PostTest.java
@@ -0,0 +1,47 @@
+package umc.blog;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import umc.blog.entity.Post;
+import umc.blog.repository.PostRepository;
+
+import javax.sql.DataSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class PostTest {
+    private final DataSource dataSource;
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostTest(DataSource dataSource, PostRepository postRepository) {
+        this.dataSource = dataSource;
+        this.postRepository = postRepository;
+    }
+
+    @Test
+    @DisplayName("글 생성 및 저장 테스트")
+    void save() {
+        // given
+        Post newPost1 = Post.builder().writer("익명").title("첫 번째 글").content("첫 번째 글 내용").build();
+        Post newPost2 = Post.builder().writer("익명").title("두 번째 글").content("두 번째 글 내용").build();
+        Post newPost3 = Post.builder().writer("익명").title("세 번째 글").content("세 번째 글 내용").build();
+
+        // when
+        postRepository.save(newPost1);
+        postRepository.save(newPost2);
+        postRepository.save(newPost3);
+        List<Post> result = postRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(3);
+    }
+}

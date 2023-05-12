e7a91028ff2594660abe6a2072884d58fbf73b21
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-05-12 13:07:36 +0900

    ✅ test: 글 수정 테스트 구현
    - 글 제목 수정을 통한 수정 테스트 구현

--- src/test/java/umc/blog/PostTest.java
+++ src/test/java/umc/blog/PostTest.java
@@ -1,12 +1,12 @@
 package umc.blog;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import umc.blog.entity.Post;
+import umc.blog.exception.TargetNotFoundException;
 import umc.blog.repository.PostRepository;
 
 import javax.sql.DataSource;
@@ -43,5 +43,21 @@ public class PostTest {
 
         // then
         assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("글 수정 테스트")
+    void edit() {
+        // given
+        Post newPost1 = Post.builder().writer("익명").title("첫 번째 글").content("첫 번째 글 내용").build();
+
+        // when
+        postRepository.save(newPost1);
+        newPost1.setTitle("첫 번째 글 (수정)");
+        Post targetPost = postRepository.findById(newPost1.getId()).orElseThrow(
+                () -> new TargetNotFoundException("target not found"));
+
+        // then
+        assertThat(targetPost.getTitle()).isEqualTo("첫 번째 글 (수정)");
     }
 }
